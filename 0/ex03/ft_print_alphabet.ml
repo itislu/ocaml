f4aca16f7476c9e570b10acf7e4074349c8f7981
Lukas <itislu.git@gmail.com>
2026-07-24 20:24:52 +0000

    feat(0/ex03): Solve

---
+++ 0/ex03/ft_print_alphabet.ml
@@ -0,0 +1,11 @@
+let ft_print_alphabet () =
+  let next c = char_of_int (int_of_char c + 1) in
+  (* shadowing c *)
+  let rec print c =
+    print_char c;
+    if c < 'z' then print (next c)
+  in
+  print 'a';
+  print_char '\n'
+
+let () = ft_print_alphabet ()
