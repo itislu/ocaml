8bc945abaf1df97913326a4e74464a3cb2d9a87e
Lukas <itislu.git@gmail.com>
2026-07-25 14:58:33 +0000

    feat(0.1/ex00): Solve

---
+++ 0.1/ex00/repeat_x.ml
@@ -0,0 +1,15 @@
+let repeat_x n =
+  let rec helper s i = if i = 0 then s else helper (s ^ "x") (i - 1) in
+  if n < 0 then "Error" else helper "" n
+
+let () =
+  let print_repeat_x n =
+    print_string (repeat_x n);
+    print_char '\n'
+  in
+  print_repeat_x (-1);
+  print_repeat_x 0;
+  print_repeat_x 1;
+  print_repeat_x 2;
+  print_repeat_x 5;
+  print_repeat_x 50000
