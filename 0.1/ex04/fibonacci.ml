516706538a80a92cc77bb32e0016bcdd9f73872e
Lukas <itislu.git@gmail.com>
2026-07-25 14:29:16 +0000

    feat(0.1/ex04): Solve

---
+++ 0.1/ex04/fibonacci.ml
@@ -0,0 +1,19 @@
+let fibonacci n =
+  let rec fib_helper n1 n2 i =
+    if i = 0 then n1 else fib_helper n2 (n1 + n2) (i - 1)
+  in
+  if n <= 0 then 0 else if n = 1 then 1 else fib_helper 0 1 n
+
+let () =
+  let print_fibonacci n =
+    print_int (fibonacci n);
+    print_char '\n'
+  in
+  print_fibonacci (-1);
+  print_fibonacci 0;
+  print_fibonacci 1;
+  print_fibonacci 2;
+  print_fibonacci 3;
+  print_fibonacci 4;
+  print_fibonacci 5;
+  print_fibonacci 20
