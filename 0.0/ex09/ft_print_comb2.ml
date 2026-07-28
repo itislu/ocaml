5e542b3565fb42639a47d4898845a9fde2888203
Lukas <itislu.git@gmail.com>
2026-07-28 19:33:04 +0200

    feat(0.0/ex09): Solve

---
+++ 0.0/ex09/ft_print_comb2.ml
@@ -0,0 +1,26 @@
+let print_num n =
+  if n < 10 then print_int 0;
+  print_int n
+
+let print n1 n2 =
+  if n2 > 1 then (
+    print_char ',';
+    print_char ' ');
+  print_num n1;
+  print_char ' ';
+  print_num n2
+
+let rec func2 n1 n2 =
+  print n1 n2;
+  if n2 < 99 then func2 n1 (n2 + 1)
+
+let rec func1 n1 =
+  let n2 = n1 + 1 in
+  func2 n1 n2;
+  if n1 < 98 then func1 n2
+
+let ft_print_comb2 () =
+  func1 0;
+  print_char '\n'
+
+let () = ft_print_comb2 ()
