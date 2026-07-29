618216c8388904302ea2bd5822d12f447ce1a917
Lukas <itislu.git@gmail.com>
2026-07-29 13:03:04 +0200

    style(0.0/ex09): Rename functions

--- 0.0/ex09/ft_print_comb2.ml
+++ 0.0/ex09/ft_print_comb2.ml
@@ -10,17 +10,17 @@ let print n1 n2 =
   print_char ' ';
   print_num n2
 
-let rec func2 n1 n2 =
+let rec inc_n2 n1 n2 =
   print n1 n2;
-  if n2 < 99 then func2 n1 (n2 + 1)
+  if n2 < 99 then inc_n2 n1 (n2 + 1)
 
-let rec func1 n1 =
+let rec inc_n1 n1 =
   let n2 = n1 + 1 in
-  func2 n1 n2;
-  if n1 < 98 then func1 n2
+  inc_n2 n1 n2;
+  if n1 < 98 then inc_n1 n2
 
 let ft_print_comb2 () =
-  func1 0;
+  inc_n1 0;
   print_char '\n'
 
 let () = ft_print_comb2 ()
