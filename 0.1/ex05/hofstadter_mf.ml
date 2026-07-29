0c2074261b7bd44d52fb53913731333aad7e7af1
Lukas <itislu.git@gmail.com>
2026-07-30 00:40:45 +0200

    perf(0.1/ex05): Speed up ~15%

--- 0.1/ex05/hofstadter_mf.ml
+++ 0.1/ex05/hofstadter_mf.ml
@@ -1,9 +1,13 @@
 let rec hfs_f n =
-  let hfs_f_nonneg n = if n = 0 then 1 else n - hfs_m (hfs_f (n - 1)) in
+  let rec hfs_f_nonneg n =
+    if n = 0 then 1 else n - hfs_m (hfs_f_nonneg (n - 1))
+  in
   if n < 0 then -1 else hfs_f_nonneg n
 
 and hfs_m n =
-  let hfs_m_nonneg n = if n = 0 then 0 else n - hfs_f (hfs_m (n - 1)) in
+  let rec hfs_m_nonneg n =
+    if n = 0 then 0 else n - hfs_f (hfs_m_nonneg (n - 1))
+  in
   if n < 0 then -1 else hfs_m_nonneg n
 
 let () =
