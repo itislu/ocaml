b31426c61ee030b366b5efdb7117fddfda58df6f
Lukas <itislu.git@gmail.com>
2026-07-30 00:13:27 +0200

    fix(0.1/ex09): Move forbidden top-level let

--- 0.1/ex09/leibniz_pi.ml
+++ 0.1/ex09/leibniz_pi.ml
@@ -1,8 +1,8 @@
-let pi = 4. *. atan 1.
-
 let leibniz_pi target_delta =
+  let pi = 4. *. atan 1. in
   let leibniz i = (-1. ** i) /. ((2. *. i) +. 1.) in
   let delta s t = if s < t then t -. s else s -. t in
+
   let rec sigma sum i =
     let res = 4. *. sum in
     if delta res pi <= target_delta then (
@@ -10,8 +10,9 @@ let leibniz_pi target_delta =
       i)
     else sigma (sum +. leibniz (float_of_int i)) (i + 1)
   in
+
   if target_delta < 0. then -1 else sigma 0. 0
 
 let () =
-  (* Printf.printf "%F\n" pi; *)
+  (* Printf.printf "%F\n" Float.pi; *)
   Printf.printf "%d\n" (leibniz_pi 0.000001)
