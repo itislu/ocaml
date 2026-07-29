307194167c1f3584db9a3eae1ef9aae6459f83d8
Lukas <itislu.git@gmail.com>
2026-07-29 23:42:58 +0200

    fix(0.1/ex02): Remove forbidden match

--- 0.1/ex02/ackermann.ml
+++ 0.1/ex02/ackermann.ml
@@ -1,9 +1,8 @@
 let ackermann m n =
   let rec ackermann_nonneg m n =
-    match (m, n) with
-    | 0, n -> n + 1
-    | m, 0 -> ackermann_nonneg (m - 1) 1
-    | m, n -> ackermann_nonneg (m - 1) (ackermann_nonneg m (n - 1))
+    if m = 0 then n + 1
+    else if n = 0 then ackermann_nonneg (m - 1) 1
+    else ackermann_nonneg (m - 1) (ackermann_nonneg m (n - 1))
   in
   if m < 0 || n < 0 then -1 else ackermann_nonneg m n
 
@@ -11,4 +10,5 @@ let () =
   ackermann (-1) 7 |> Printf.printf "%d\n";
   ackermann 0 0 |> Printf.printf "%d\n";
   ackermann 2 3 |> Printf.printf "%d\n";
+  flush stdout;
   ackermann 4 1 |> Printf.printf "%d\n"
