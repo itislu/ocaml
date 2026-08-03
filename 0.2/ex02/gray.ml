0c23918a9fe45e5ee66e80fd33612a7e90cbfdfd
Lukas <itislu.git@gmail.com>
2026-08-03 18:12:13 +0200

    chore(0.2/ex02): Move comments to bottom

--- 0.2/ex02/gray.ml
+++ 0.2/ex02/gray.ml
@@ -1,23 +1,27 @@
-(*
-00000
-00001
-00011
-00010
-00110
-00111
-00101
-00100
-01100
-01101
-01111
-01110
-01010
-01011
-01001
-01000
-11000
-11001
+let flip = function '0' -> '1' | '1' -> '0' | c -> c
+let flipi i_toflip i_cur c = if i_cur = i_toflip then flip c else c
+let fst (a, _) = a
 
+let gray n =
+  if n < 0 then print_endline "Error"
+  else
+    let rec generate i acc s =
+      if i = n then (acc ^ " " ^ s, s)
+      else
+        let acc, s = generate (i + 1) acc s in
+        generate (i + 1) acc (String.mapi (flipi i) s)
+    in
+    String.make n '0' |> generate 0 "" |> fst |> String.trim |> print_endline
+
+let () =
+  gray (-1);
+  gray 0;
+  gray 1;
+  gray 2;
+  gray 3;
+  gray 4
+
+(*
 0000
 0001 3
 0011 2
@@ -44,7 +48,7 @@
 00101 3
 00100 4
 01100 1
-01101 4 <
+01101 4
 01111 3
 01110 4
 01010 2
@@ -53,41 +57,18 @@
 01000 4
 11000 0
 11001 4
-      3
-      4
-      2
-      4
-      3
-      4
-      1
-      4
-      3
-      4
-      2
-      4
-      3
-      4
+11011 3
+11010 4
+11110 2
+11111 4
+11101 3
+11100 4
+10100 1
+10101 4
+10111 3
+10110 4
+10010 2
+10011 4
+10001 3
+10000 4
 *)
-
-let flip = function '0' -> '1' | '1' -> '0' | c -> c
-let flipi i_toflip i_cur c = if i_cur = i_toflip then flip c else c
-let fst (a, _) = a
-
-let gray n =
-  if n < 0 then print_endline "Error"
-  else
-    let rec generate i acc s =
-      if i = n then (acc ^ " " ^ s, s)
-      else
-        let acc, s = generate (i + 1) acc s in
-        generate (i + 1) acc (String.mapi (flipi i) s)
-    in
-    String.make n '0' |> generate 0 "" |> fst |> String.trim |> print_endline
-
-let () =
-  gray (-1);
-  gray 0;
-  gray 1;
-  gray 2;
-  gray 3;
-  gray 4
